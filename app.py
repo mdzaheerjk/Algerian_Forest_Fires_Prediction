683efd13144c195700870068fc6f21be45d40d9f
zaheer <183212672+mdzaheerjk@users.noreply.github.com>
2026-04-13 13:20:48 +0530

    did standard scaler

--- app.py
+++ app.py
@@ -20,20 +20,24 @@ st.title("🔥Algerian Forest Fire Prediction")
 columns1,columns2=st.columns(2)
 
 with columns1:
-    TEMPERATURE=st.number_input("Temperature")
-    RH=st.number_input("Relative Humidity (RH)")
-    WS=st.number_input("Wind Speed")
-    RAIN=st.number_input("Rain")
-    FFMC=st.number_input("FFMC")
+    TEMPERATURE=st.number_input("Temperature",max_value=42,min_value=22,value=32)
+    RH=st.number_input("Relative Humidity (RH)",max_value=90,min_value=21,value=62)
+    WS=st.number_input("Wind Speed",max_value=29,min_value=6,value=15)
+    RAIN=st.number_input("Rain",max_value=17,min_value=0,value=1,)
+    FFMC=st.number_input("FFMC",max_value=96,min_value=28,value=77)
 with columns2:
-    DMC=st.number_input("DMC")
-    ISI=st.number_input("ISI")
-    CLASSES=st.number_input("Classes")
-    REGION=st.number_input("Region")
+    DMC=st.number_input("DMC",max_value=65,min_value=0,value=14)
+    ISI=st.number_input("ISI",value=4,min_value=0,max_value=19)
+    CLASSES=st.selectbox("Select an Option",options=[0,1])
+    REGION=st.selectbox("Select an option",options=[0,1])
+    
 
 input_data=np.array([[TEMPERATURE,RH,WS,RAIN,FFMC,DMC,ISI,CLASSES,REGION]])
-scaled=scaler.fit_transform(input_data)
+scaled=scaler.transform(input_data)
 result=model.predict(scaled)
+
+
+st.write(scaled)
 
 if st.button("Predict",type='primary'):
     st.success(result[0])
